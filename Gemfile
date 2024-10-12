8795a833233d005d1ee2522c24a0993a7c43e2b1
Kyle Verwey <108106818+starzzots@users.noreply.github.com>
2024-10-12 21:13:45 +0000

    finished last part had to use the solutions help wont lie

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 
 gem "http"
 gem "json"
+gem "ascii_charts"
--- umbrella.rb
+++ umbrella.rb
@@ -41,4 +41,30 @@ temp = current_weather.fetch("temperature")
 
 puts "It is currently #{temp}°F." #hold alt + 0176 = ° 
 
+hourly_weather = weather_resp_parsed.fetch("hourly")
 
+hourly_data = hourly_weather.fetch("data")
+any_precipitation = false
+precip_prob_threshold = 0.10
+hourly_data.each do |hour_hash|
+
+  precip_prob = hour_hash.fetch("precipProbability")
+
+  if precip_prob > precip_prob_threshold
+    any_precipitation = true
+
+    precip_time = Time.at(hour_hash.fetch("time"))
+
+    seconds_from_now = precip_time - Time.now
+
+    hours_from_now = seconds_from_now / 60 / 60
+
+    puts "In #{hours_from_now.round} hours, there is a #{(precip_prob * 100).round}% chance of precipitation."
+  end
+end
+
+if any_precipitation == true
+  puts "You might want to take an umbrella!"
+else
+  puts "You probably won't need an umbrella."
+end
