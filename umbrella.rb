54dffb2676f164eeb3d8554023380a638cf68d6b
Kyle Verwey <108106818+starzzots@users.noreply.github.com>
2024-10-12 20:44:12 +0000

    current tempeture working

--- umbrella.rb
+++ umbrella.rb
@@ -3,9 +3,9 @@ require "json"
 
 puts "Where are you located?"
 
-user_location = gets.chomp
-#user_location = "Chicago"
-pp user_location
+#user_location = gets.chomp
+user_location = "Chicago"
+puts "Checking weather at #{user_location}..."
 
 maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=#{ENV.fetch("GMAPS_KEY")}"
 
@@ -26,5 +26,19 @@ loc = gemoetry.fetch("location")
 lat = loc.fetch("lat")
 lng = loc.fetch("lng")
 
-pp lat
-pp lng
+puts "Your coordinates are Latitude:#{lat}, Longitude:#{lng}"
+
+weather_url="https://api.pirateweather.net/forecast/#{ENV.fetch("PIRATE_WEATHER_API_KEY")}/#{lat},#{lng}"
+
+weather_resp = HTTP.get(weather_url)
+weather_resp_raw = weather_resp.to_s
+
+weather_resp_parsed = JSON.parse(weather_resp_raw)
+
+current_weather = weather_resp_parsed.fetch("currently")
+
+temp = current_weather.fetch("temperature")
+
+puts "It is currently #{temp}°F." #hold alt + 0176 = ° 
+
+
